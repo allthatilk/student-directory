08830075fad530ed0bc9577f938c0c7da2514a8e
allthatilk <imogenkutz@hotmail.com>
2017-02-01 13:02:14 +0000

    Part 8 exercise 5 half working

--- directory.rb
+++ directory.rb
@@ -1,16 +1,29 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the profiles of the students"
   puts "To finish, just hit return twice"
   # Create an empty array
   students = []
+  hobbies = []
+  heights = []
+  most_likely_to = []
+  #cohort = []
   # Get the first name
+  puts "Please enter the student's name: "
   name = gets.chomp
+  puts "Please enter their favourite hobby: "
+  hobby = gets.chomp
+  puts "Please enter their height: "
+  height = gets.chomp
+  puts "What are they most likely to be remembered for?"
+  activity = gets.chomp
   # While the name is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array
-    students << {name: name, cohort: :november}
+    #profile <<
+    students << {name: name, hobbies: hobby, heights: height, most_likely_to: activity, cohort: :november}
     puts "Now we have #{students.count} students"
     # Get another name from the user
+    puts "Please enter the next student's name: "
     name = gets.chomp
   end
   # Return the array of students
@@ -33,7 +46,7 @@ def print(results)
   tally = 0
   while tally < results.count
     index = results[tally]
-     puts "#{tally}. #{index[:name]} (#{index[:cohort]} cohort)"
+     puts "#{tally}. #{index[:name]}\n\tFavourite pastime: #{index[:hobby]}\n\tHeight: #{index[:height]}\n\tMost Likely to resort to #{index[:most_likely_to]}\n\t(#{index[:cohort]} cohort)"
     tally +=1
   end
 end
@@ -43,7 +56,7 @@ def print_footer(names)
 end
 # Takes user input to determine the letter to filter students by
 def initial_choice
-  puts "Please provide a first initial for us to filter your results: "
+  puts "Please provide a first initial for us to filter your results , or press ENTER again to skip: "
   filter_by = gets.chomp
 end
 # Nothing happens until we call methods

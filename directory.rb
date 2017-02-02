9c3bfa80f86dc5955ba2849f71802c77c511244a
allthatilk <imogenkutz@hotmail.com>
2017-02-02 04:33:56 +0000

    Part8 exercise8 completed

--- directory.rb
+++ directory.rb
@@ -44,31 +44,33 @@ def input_cohort(students, cohort = "No")
       edit = gets.chomp.downcase
 
         if edit == "y" or edit == "yes"
+          student[:cohort] = cohort
           puts "#{student[:name]} has been registered to the #{cohort} cohort"
         elsif edit ==  "n" or edit == "no"
           puts "Please enter the correct cohort: "
           cohort = gets.chomp
         end
 
-     end
+    end
 
-    students.map { |student| student[:cohort] = cohort}
   end
 
   students
 end
 
 def input_student_info(students)
-
+# Takes input and adds to each student's hash in array
   students.each do |student|
     puts "Please enter #{student[:name]}'s favourite hobby: "
     hobby = gets.chomp
+    student[:hobbies] = hobby
     puts "Please enter #{student[:name]}'s height: "
     height = gets.chomp
+    student[:heights] = height
     puts "What is #{student[:name]} most likely to be remembered for?"
     activity = gets.chomp
-    # Add the student hash to the array
-    students.map { |student| student[:hobbies] = hobby; student[:heights] = height; student[:most_likely_to] = activity}
+    student[:most_likely_to] = activity
+
     puts "Now we have #{students.count} students"
   end
   students
@@ -80,25 +82,39 @@ def print_header
 end
 # Takes an argument to filter students, creating a new array to be iterated
 # over for printing
-def filter(students_arr, filter_by)
-  specified = students_arr.select { |student|
-    student[:name].start_with?(filter_by) && student[:name].length < 12}
-end
+#def filter(students_arr, filter_by)
+#  specified = students_arr.select { |student|
+#    student[:name].start_with?(filter_by) && student[:name].length < 12}
+#end
 # Creates a tally and uses that as index for results array values to iterate
 # through results to print
-def print(results)
-  tally = 0
+#def print(results)
+#  tally = 0
 
-  while tally < results.count
-    index = results[tally]
-     puts "#{tally + 1}. #{index[:name]}".center(70)
-     puts "Favourite hobby: #{index[:hobbies]}".center(75)
-     puts "Height: #{index[:heights]}".center(75)
-     puts "Most likely to resort to #{index[:most_likely_to]}".center(75)
-     puts "(#{index[:cohort]} cohort)".center(75)
-    tally +=1
-  end
+#  while tally < results.count
+#    index = results[tally]
+#     puts "#{tally + 1}. #{index[:name]}".center(70)
+#     puts "Favourite hobby: #{index[:hobbies]}".center(75)
+#     puts "Height: #{index[:heights]}".center(75)
+#     puts "Most likely to resort to #{index[:most_likely_to]}".center(75)
+#     puts "(#{index[:cohort]} cohort)".center(75)
+#    tally +=1
+#  end
 
+#end
+
+def print_by_cohort(students)
+  puts "Please select the cohort you wish to view: "
+  filter = gets.chomp.to_sym
+
+  students.select { |student|
+    student[:cohort] == filter }.each_with_index {|student, index|
+      puts "This is the #{student[:cohort]} cohort: "
+      puts "#{index + 1}. #{student[:name]}".center(70)
+      puts "Favourite hobby: #{student[:hobbies]}".center(75)
+      puts "Height: #{student[:heights]}".center(75)
+      puts "Most likely to resort to #{student[:most_likely_to]}".center(75)
+      }
 end
 
 def print_footer(names)
@@ -118,6 +134,7 @@ end
 # Nothing happens until we call methods
 students_arr = input_student_info(input_cohort(input_name))
 print_header
-results = filter(students_arr, initial_choice)
-print(results)
+print_by_cohort(students_arr)
+#results = filter(students_arr, initial_choice)
+#print(results)
 print_footer(students_arr)

08574a82054ae69b66d4fb0ad1d0ecf57e60287b
allthatilk <imogenkutz@hotmail.com>
2017-02-02 00:47:42 +0000

    P8 ex7 ongoing but working

--- directory.rb
+++ directory.rb
@@ -1,32 +1,50 @@
-def input_students
-  puts "Please enter the profiles of the students"
-  puts "To finish, just hit return twice"
+def input_name(name = "J. Doe")
+  puts "Please enter the name of the student."
+  puts "To finish, just hit return twice."
   # Create an empty array
   students = []
   # Sets name variable to empty for while loop to work
-  name = " "
-  # While the name is empty, repeat this code
-  while !name.empty? do
+  # While the name is not empty, repeat this code
+  until name.empty? do
   # Get the first name
-    puts "Please enter the student's name: "
     name = gets.chomp
-    # If a name is entered it loops for more info to add to the student hash
+    # If a name is entered it adds the name has to the students array and counts
+    # the number of students entered so far
     if name.empty? == false
-      puts "Please enter their favourite hobby: "
-      hobby = gets.chomp
-      puts "Please enter their height: "
-      height = gets.chomp
-      puts "What are they most likely to be remembered for?"
-      activity = gets.chomp
-      puts "Which cohort do they belong to?"
-      cohort = gets.chomp
-      # Add the student hash to the array
-      students << {name: name, hobbies: hobby, heights: height, most_likely_to: activity, cohort: cohort}
+      students << {name: name}
       puts "Now we have #{students.count} students"
+      puts "You can enter the name of another student or press ENTER to skip"
     end
 
   end
   # Return the array of students
+  students
+end
+
+def input_cohort(students, cohort = "No")
+
+  students.each do |student|
+    puts "Which cohort does #{student[:name]} belong to?"
+    cohort = gets.chomp.to_sym
+    students.map { |student| student[:cohort] = cohort}
+  end
+
+  students
+end
+
+def input_student_info(students)
+
+  students.each do |student|
+    puts "Please enter #{student[:name]}'s favourite hobby: "
+    hobby = gets.chomp
+    puts "Please enter #{student[:name]}'s height: "
+    height = gets.chomp
+    puts "What is #{student[:name]} most likely to be remembered for?"
+    activity = gets.chomp
+    # Add the student hash to the array
+    students.map { |student| student[:hobbies] = hobby; student[:heights] = height; student[:most_likely_to] = activity}
+    puts "Now we have #{students.count} students"
+  end
   students
 end
 
@@ -36,8 +54,8 @@ def print_header
 end
 # Takes an argument to filter students, creating a new array to be iterated
 # over for printing
-def filter(students, filter_by)
-  specified = students.select { |student|
+def filter(students_arr, filter_by)
+  specified = students_arr.select { |student|
     student[:name].start_with?(filter_by) && student[:name].length < 12}
 end
 # Creates a tally and uses that as index for results array values to iterate
@@ -72,8 +90,8 @@ def initial_choice
   filter_by = gets.chomp
 end
 # Nothing happens until we call methods
-students = input_students
+students_arr = input_student_info(input_cohort(input_name))
 print_header
-results = filter(students, initial_choice)
+results = filter(students_arr, initial_choice)
 print(results)
-print_footer(students)
+print_footer(students_arr)
